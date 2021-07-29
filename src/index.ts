9e4cd4e498a31c44aab6ae637f8b1157b8719336
Boris <rozumniy.boris.work@gmail.com>
2021-07-29 12:33:28 +0300

    chore(config-project): remove comments

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-// // import express from 'express';
-// const express = require('express');
-
-// const app = express();
-// const port = 5000;
-// app.get('/', (request, response) => {
-//   response.send('Hello world!');
-// });
-// app.listen(port, () => console.log(`Running on port ${port}`));
-
 import express from "express";
 const app = express();
 const port = 8080; // default port to listen
