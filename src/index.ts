531a0644f62fa3ce1766d917561e39d039ee06eb
Boris <rozumniy.boris.work@gmail.com>
2021-08-05 12:43:22 +0300

    romve comments

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 const app = express();
-const port = 8080; // default port to listen
-// define a route handler for the default home page
+const port = 8080;
+
 app.get('/', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.send('Hello world from server!');
 });
 
-// start the Express server
 app.listen(port, () => {
   // tslint:disable-next-line:no-console
   console.log(`Server started at http://localhost:${port}`);
