3b26366a33e652ff730c3f2e9ab87cfba1d5e754
Boris <rozumniy.boris.work@gmail.com>
2021-08-05 12:23:40 +0300

    draft commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
+import { useHttp } from './hooks/http.hook';
 import './App.css';
+import { json } from 'express';
 
 function App() {
+  useEffect(() => {
+    console.log('start');
+  }, []);
+
+  const getPages = useCallback(async () => {
+    try {
+      const response = await axios.get('http://localhost:8080');
+      // setPages(response.data);
+    } catch (error) {
+      // setPages(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getPages();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +38,7 @@ function App() {
         >
           Learn React
         </a>
+        {/* <input onChange={(e) => pressHandler} /> */}
       </header>
     </div>
   );
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
-import express from "express";
+import express from 'express';
 const app = express();
 const port = 8080; // default port to listen
-
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!!" );
-} );
+app.get('/', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.send('Hello world from server!');
+});
 
 // start the Express server
-app.listen( port, () => {
-    // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`Server started at http://localhost:${port}`);
+});
+
+
