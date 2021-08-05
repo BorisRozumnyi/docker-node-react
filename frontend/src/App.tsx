fc2c61b706ac90a6b85e3fa2c5c173b658b7732b
Boris <rozumniy.boris.work@gmail.com>
2021-08-05 12:38:24 +0300

    display response from server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,21 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
-import { useHttp } from './hooks/http.hook';
 import './App.css';
-import { json } from 'express';
 
 function App() {
-  useEffect(() => {
-    console.log('start');
-  }, []);
-
-  const getPages = useCallback(async () => {
+  const [data, setData] = useState();
+  const getData = useCallback(async () => {
     try {
       const response = await axios.get('http://localhost:8080');
-      // setPages(response.data);
+      setData(response.data);
     } catch (error) {
-      // setPages(error);
+      setData(error);
     }
   }, []);
 
   useEffect(() => {
-    getPages();
+    getData();
   }, []);
 
   return (
@@ -28,17 +23,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        {data && data}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {/* <input onChange={(e) => pressHandler} /> */}
       </header>
     </div>
   );
